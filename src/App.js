f8b18d564306fee695bb794106d6187d232b4e19
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-19 22:01:33 +0530

    todo app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import JSONDATA from "./JSONDATA.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [inputChange, setInputChange] = useState("");
+
+  // useEffect(() => {
+  //   setItems(JSONDATA);
+  // }, []);
+
+  // Taking input from the users
+  const InputChangeHandler = (e) => {
+    setInputChange(e.target.value);
+  };
+
+  // add data
+  const addHandler = () => {
+    const newItems = { task: inputChange };
+    setItems([...items, newItems]);
+    setInputChange("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="outerClass">
+        <h1>ToDo App</h1>
+        {items.map((val, id) => {
+          return (
+            <div key={id}>{val.task}</div>
+          );
+        })}
+        <div>
+          <input
+            onChange={InputChangeHandler}
+            placeholder="add your task here"
+            value={inputChange}
+          />
+        </div>
+        <button onClick={addHandler}>Add</button>
+      </div>
     </div>
   );
 }
